82423606fb4f430e482b9a748a81011cc62760dd
Brian Scarlett <bs2525@bath.ac.uk>
2026-05-30 23:37:51 -0400

    fixed version numbering

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import sys
 
 from asset_creation import create_asset
+from asset_browser import get_assets
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import(QLabel, QVBoxLayout, QLineEdit, QComboBox, QDoubleSpinBox, QPushButton, QMainWindow, QApplication, QWidget)
+from PySide6.QtWidgets import(QLabel, QVBoxLayout, QLineEdit, QComboBox, QPushButton, QMainWindow, QApplication, QTabWidget, QWidget, QTableWidget, QTableWidgetItem, QHeaderView)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -11,7 +12,14 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("VFX Asset Pipeline")
 
+        
+        #create a tab widget
+        tab = QTabWidget(self)
+
+        #Asset Creation Tab
+        asset_info = QWidget(self)
         layout = QVBoxLayout()
+        asset_info.setLayout(layout)
 
         self.textbox = QLineEdit()
         self.textbox.setMaxLength(10)
@@ -32,21 +40,51 @@ class MainWindow(QMainWindow):
         layout.addWidget(QLabel("Asset LOD"))
         layout.addWidget(self.comboboxLOD)
 
-
-
         self.Btn = QPushButton(text="Create Asset", parent=self)
         self.Btn.setFixedSize(100, 60)
         self.Btn.clicked.connect(self.create_asset)
 
         layout.addWidget(self.Btn)
 
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
 
-        self.setCentralWidget(central_widget)
+        #Browser Tab
+        browser = QWidget(self)
+        layout = QVBoxLayout()
+        browser.setLayout(layout)
+
+        assets = get_assets()
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setRowCount(len(assets))
+        self.table.setHorizontalHeaderLabels(["Name", "Type", "Version", "Date Created"])
+
+        for row_index, asset in enumerate(assets):
+            self.table.setItem(row_index, 0, QTableWidgetItem(asset["Name"]))
+            self.table.setItem(row_index, 1, QTableWidgetItem(asset["Type"]))
+            self.table.setItem(row_index, 2, QTableWidgetItem(str(asset["Version"])))
+            self.table.setItem(row_index, 3, QTableWidgetItem(asset["Date"]))
+        
+        self.table.resizeColumnsToContents()
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self.table.verticalHeader().setVisible(False)
+
+        layout.addWidget(self.table)
+
+    
+
+
+
+        self.setCentralWidget(tab)
+
+        tab.addTab(asset_info, 'Create an Asset')
+        tab.addTab(browser, 'Asset Browser')
 
     def create_asset(self):
         create_asset(self.textbox.text(), self.combobox.currentText(), self.comboboxLOD.currentText())
+
+    def get_assets(self):
+        get_assets(self.textbox.text(), self.combobox.currentText(), self.comboboxLOD.currentText())
 
 
 
--- asset_browser.py
+++ asset_browser.py
@@ -1,25 +1,34 @@
 import os
 import datetime
 
-for root, dirs, files in os.walk('Assets'):
+def get_assets():
 
-    ver_list=[]
+    assets = []
 
-    if files:
-        typename = root.split("\\")
-        asset_type = typename[1]
-        asset_name = typename[2]
+    for root, dirs, files in os.walk('Assets'):
+
+
+        if files:
+            typename = root.split("\\")
+            asset_type = typename[1]
+            asset_name = typename[2]
        
-        for version_number in files:
-            first_split = version_number.split(f'{asset_name}_v')
-            second_split = first_split[1].split(".usda")
+            for version_number in files:
+                first_split = version_number.split(f'{asset_name}_v')
+                second_split = first_split[1].split(".usda")
 
-            current_version = float(second_split[0])
+                current_version = float(second_split[0])
 
-            ver_list.append(current_version)
+                date_created_unix = os.path.getctime(f"{root}\\{version_number}")
+                timestamp = datetime.datetime.fromtimestamp(date_created_unix)
+                date_created = (timestamp.strftime('%d-%m-%Y  %H:%M:%S'))
+
+                assets.append({"Name": asset_name, "Type": asset_type, "Version": current_version, "Date": date_created})
         
-        date_created_unix = os.path.getctime(f"{root}\\{version_number}")
-        timestamp = datetime.datetime.fromtimestamp(date_created_unix)
-        date_created = (timestamp.strftime('%d-%m-%Y  %H:%M:%S'))
-        print(f"Name: {asset_name} | Type: {asset_type} | Version: {ver_list} | Date Created: {date_created}")
+        
+
+
+    
+    return assets
+        
       
